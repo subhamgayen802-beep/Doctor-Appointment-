3977a4f139a54737383bd238e58cc9cf257c7a81
Subham Gayen <subhamgayen802@gmail.com>
2026-04-06 23:13:45 +0530

    tenth commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://doctor-appointment-fskb2r41h-subhamgayen802-4438s-projects.vercel.app/"
   ],
   credentials: true
 }));
