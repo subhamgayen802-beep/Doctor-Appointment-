844a0fbdd6ec9bed0bc5fbf3fdbc08433691a319
Subham Gayen <subhamgayen802@gmail.com>
2026-04-06 17:25:47 +0530

    render fix

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -2,7 +2,7 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
     username: 'default',
-    password: '0cgiOrXYbv4857lZi5oYLVoPy0HcwTkl',
+    password: process.env.REDIS_PASSWORD,
     socket: {
         host: 'redis-17809.c8.us-east-1-2.ec2.cloud.redislabs.com',
         port: 17809
@@ -10,26 +10,5 @@ const redisClient = createClient({
 });
 
 module.exports =redisClient;
-
-
-
-// import { createClient } from 'redis';
-
-// const client = createClient({
-//     username: 'default',
-//     password: '0cgiOrXYbv4857lZi5oYLVoPy0HcwTkl',
-//     socket: {
-//         host: 'redis-17809.c8.us-east-1-2.ec2.cloud.redislabs.com',
-//         port: 17809
-//     }
-// });
-
-// client.on('error', err => console.log('Redis Client Error', err));
-
-// await client.connect();
-
-// await client.set('foo', 'bar');
-// const result = await client.get('foo');
-// console.log(result)  // >>> bar
 
 
