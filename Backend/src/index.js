e2dae4256bfd51aab1f914f96d82f23b88a32ace
Subham Gayen <subhamgayen802@gmail.com>
2026-06-13 16:12:23 +0530

    backend fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://vercel.com/subhamgayen802-4438s-projects/doctor-appointment',
     credentials: true,
 }));
 
